57eb9482eae47fdba89ddc2cb4649d0d8eb190c8
“justintangjx” <just.think.tang@gmail.com>
2018-02-28 16:36:53 +0800

    3rd test

--- test/kebabize.test.js
+++ test/kebabize.test.js
@@ -4,11 +4,11 @@ test("hiThere should be kebabized to hi-there", function() {
   expect(kebabize("hiThere")).toEqual("hi-there");
 });
 
-test.only("hiThereJustin should be kebabized to hi-there-justin", function() {
+test("hiThereJustin should be kebabized to hi-there-justin", function() {
   expect(kebabize("hiThereJustin")).toEqual("hi-there-justin");
 });
 
-test.skip("hiThereBruceWayne shuld be kebabized to hi-there-bruce-wayne", function() {
+test("hiThereBruceWayne shuld be kebabized to hi-there-bruce-wayne", function() {
   expect(kebabize("hiThereBruceWayne")).toEqual("hi-there-bruce-wayne");
 });
 
